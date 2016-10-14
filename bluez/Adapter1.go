7f5198c04f1fcb585ddf1665daf3d21d496d2687
Luca Capra <luca.capra@gmail.com>
2016-10-15 00:16:43 +0200

    fix call return

--- bluez/Adapter1.go
+++ bluez/Adapter1.go
@@ -1,14 +1,23 @@
 package bluez
 
 import (
+	"github.com/godbus/dbus"
 	"github.com/muka/device-manager/client"
 	"log"
 )
 
 // NewAdapter1 create a new Adapter1 client
-func NewAdapter1(idx int) *Adapter1 {
+func NewAdapter1(idx string) *Adapter1 {
 	a := new(Adapter1)
-	a.client = client.NewClient("org.bluez.Adapter1", "/org/bluez/hci"+string(idx))
+	a.client = client.NewClient(
+		&client.Config{
+			Name:  "org.bluez",
+			Iface: "org.bluez.Adapter1",
+			Path:  "/org/bluez/hci" + idx,
+			Bus:   client.SystemBus,
+		},
+	)
+	a.client.Connect()
 	return a
 }
 
@@ -20,12 +29,15 @@ type Adapter1 struct {
 
 //StartDiscovery on the adapter
 func (a *Adapter1) StartDiscovery() error {
-	err := a.client.Call("StartDiscovery", 0).Store()
-	return err
+	return a.client.Call("StartDiscovery", 0).Store()
 }
 
 //StopDiscovery on the adapter
 func (a *Adapter1) StopDiscovery() error {
-	err := a.client.Call("StartDiscovery", 0).Store()
-	return err
+	return a.client.Call("StopDiscovery", 0).Store()
+}
+
+//RemoveDevice from the list
+func (a *Adapter1) RemoveDevice(device string) error {
+	return a.client.Call("RemoveDevice", 0, dbus.ObjectPath(device)).Store()
 }
--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"github.com/muka/bluez-client/bluez"
+)
+
 func main() {
-	fmt.Println("Ciao")
+
+	hci0 := bluez.NewAdapter1("0")
+
+	fmt.Println("Started discovery")
+	hci0.StartDiscovery()
+
+	fmt.Println("Stop discovery")
+	hci0.StopDiscovery()
+
+	fmt.Println("RM dev")
+	hci0.RemoveDevice("device")
+
 }
