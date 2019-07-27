b117e3c20dad7166ccac6eae18d5cd538ff932dd
Luca Capra <luca.capra@gmail.com>
2019-07-27 08:33:12 +0200

    added initializer docs

--- gen/generator_constructors.go
+++ gen/generator_constructors.go
@@ -14,72 +14,13 @@ func isDefaultService(s string) bool {
 
 func createConstructors(api Api) []Constructor {
 
-	constructors := []Constructor{}
-
 	// log.Debugf("-------------------------------------- %s", api.Interface)
-	// log.Debugf("ObjectPath %s", api.ObjectPath)
-	// log.Debugf("Interface %s", api.Interface)
 
-	apiService := api.Service
-	if apiService != "" {
-		apiService = strings.Split(apiService, " ")[0]
-	}
-
-	if !isDefaultService(apiService) {
-
-		// log.Debugf("Service %s", api.Service)
-
-		// case 1
-		// unique name (Target role)
-		// org.bluez (Controller role)
-		if strings.Contains(apiService, "\n") {
-
-			re := regexp.MustCompile(`(.+) \((.+?) .+\)`)
-			matches1 := re.FindAllSubmatch([]byte(apiService), -1)
-
-			for _, m1 := range matches1 {
-
-				doc := ""
-				srvc := strings.Trim(string(m1[1]), " \t")
-				if !isDefaultService(srvc) {
-					doc = srvc
-					srvc = ""
-				}
-
-				c := Constructor{
-					Service: srvc,
-					Role:    string(m1[2]),
-					Docs: []string{
-						"servicePath: " + doc,
-					},
-				}
-
-				constructors = append(constructors, c)
-			}
-		} else {
-			c := Constructor{
-				Service: "",
-				Role:    "",
-				Docs: []string{
-					"servicePath: " + api.Service,
-				},
-			}
-			constructors = append(constructors, c)
-		}
-		// log.Debugf("%++v", constructors)
-	} else {
-		c := Constructor{
-			Service:    apiService,
-			Role:       "",
-			ObjectPath: "",
-			Args:       "",
-		}
-		constructors = append(constructors, c)
-	}
+	constructors := []Constructor{}
+	constructors = inspectServiceName(api.Service, constructors)
+	constructors = inspectObjectPath(api.ObjectPath, constructors)
 
 	for i, c := range constructors {
-
-		inspectObjectPath(api.ObjectPath, &c)
 
 		args := []string{}
 		if c.Service == "" {
@@ -101,10 +42,15 @@ func createConstructors(api Api) []Constructor {
 		docs := []string{}
 		for _, doc := range c.Docs {
 			for _, d1 := range strings.Split(doc, "\n") {
-				docs = append(docs, "// "+d1)
+				docs = append(docs, "// \t"+d1)
 			}
 		}
-		c.ArgsDocs = strings.Join(docs, "\n")
+		c.ArgsDocs = "//\n// Args:\n" + strings.Join(docs, "\n")
+
+		// target as the deafult initializer
+		if c.Role == "Target" {
+			c.Role = ""
+		}
 
 		constructors[i] = c
 	}
@@ -114,56 +60,154 @@ func createConstructors(api Api) []Constructor {
 	return constructors
 }
 
-func inspectObjectPath(objectPath string, c *Constructor) {
+func inspectServiceName(serviceName string, constructors []Constructor) []Constructor {
 
-	if strings.Contains(objectPath, "\n") {
+	// log.Debugf("ObjectPath %s", api.ObjectPath)
+	// log.Debugf("Interface %s", api.Interface)
 
-		// log.Debugf("ObjectPath: \n----\n%s\n\n-----", objectPath)
+	apiService := serviceName
+	if apiService != "" {
+		apiService = strings.Split(apiService, " ")[0]
+	}
 
-		anchor1 := "(Target role)"
-		idx := strings.Index(objectPath, anchor1)
-		if idx > -1 {
+	if !isDefaultService(apiService) {
 
-			target := objectPath[:idx]
+		// log.Debugf("Service %s", apiService)
 
-			anchor2 := "(Controller role)"
-			idx2 := strings.Index(objectPath, anchor2)
-			controller := objectPath[idx+len(anchor1) : idx2]
+		// case 1
+		// unique name (Target role)
+		// org.bluez (Controller role)
+		if strings.Contains(serviceName, "\n") {
 
-			target = strings.Replace(strings.Trim(target, " \t\n"), "\n", "", -1)
-			controller = strings.Replace(strings.Trim(controller, " \t\n"), "\n\t", "", -1)
+			re := regexp.MustCompile(`(.+) \((.+?) .+\)`)
+			matches1 := re.FindAllSubmatch([]byte(serviceName), -1)
 
-			// log.Debugf("target %s", target)
-			// log.Debugf("controller %s", controller)
-			// log.Debugf("ROLE %s", c.Role)
+			// log.Debugf("%s ----> %s", serviceName, matches1)
 
-			c.Docs = append(c.Docs, "objectPath: "+objectPath)
+			for _, m1 := range matches1 {
 
-			if c.Role == "Target" {
-				c.ObjectPath = target
+				doc := ""
+				srvc := strings.Trim(string(m1[1]), " \t")
+
+				if !isDefaultService(srvc) {
+					doc = srvc
+					srvc = ""
+				}
+
+				docslist := []string{}
+				if doc != "" {
+					docslist = append(docslist, "servicePath: "+doc)
+				}
+
+				c := Constructor{
+					Service: srvc,
+					Role:    string(m1[2]),
+					Docs:    docslist,
+				}
+
+				constructors = append(constructors, c)
+			}
+		} else {
+
+			c := Constructor{
+				Service: "",
+				Role:    "",
+				Docs: []string{
+					"servicePath: " + serviceName,
+				},
+			}
+			constructors = append(constructors, c)
+		}
+	} else {
+		c := Constructor{
+			Service:    apiService,
+			Role:       "",
+			ObjectPath: "",
+			Args:       "",
+			Docs:       []string{},
+		}
+		constructors = append(constructors, c)
+	}
+
+	return constructors
+}
+
+func inspectObjectPath(objectPath string, constructors []Constructor) []Constructor {
+
+	constructors2 := []Constructor{}
+
+	for _, c := range constructors {
+
+		if strings.Contains(objectPath, "\n") {
+
+			// log.Debugf("ObjectPath: \n----\n%s\n\n-----", objectPath)
+
+			anchor1 := " (Target role)"
+			idx := strings.Index(objectPath, anchor1)
+			if idx > -1 {
+
+				target := objectPath[:idx]
+
+				anchor2 := "(Controller role)"
+				idx2 := strings.Index(objectPath, anchor2)
+				controller := objectPath[idx+len(anchor1) : idx2]
+
+				target = strings.Replace(strings.Trim(target, " \t\n"), "\n", "", -1)
+				controller = strings.Replace(strings.Trim(controller, " \t\n"), "\n\t", "", -1)
+
+				// if Role is set apply a objectPath
+				if c.Role == "Target" {
+					c.ObjectPath = ""
+					c.Docs = append(c.Docs, "objectPath: "+target)
+				}
+
+				if c.Role == "Controller" {
+					c.ObjectPath = ""
+					c.Docs = append(c.Docs, "objectPath: "+controller)
+				}
+
+				// if no Role, create a contructor for each objectPath
+				if c.Role == "" {
+
+					if controller != "" {
+						c1 := c
+						c1.Role = "Controller"
+						c1.ObjectPath = ""
+						c1.Docs = append(c1.Docs, "objectPath: "+controller)
+						constructors2 = append(constructors2, c1)
+					}
+
+					if target != "" {
+						c1 := c
+						c1.Role = "Target"
+						c1.ObjectPath = ""
+						c1.Docs = append(c1.Docs, "objectPath: "+target)
+						constructors2 = append(constructors2, c1)
+					}
+
+					continue
+				}
+
 			}
 
-			if c.Role == "Controller" {
-				c.ObjectPath = controller
-			}
-
+			constructors2 = append(constructors2, c)
+			continue
 		}
 
-		return
+		// freely definable
+		if strings.Contains(objectPath, "freely") {
+			c.ObjectPath = ""
+			c.Docs = append(c.Docs, "objectPath: "+objectPath)
+		}
+
+		// <application dependent>
+		if strings.HasPrefix(objectPath, "<application") {
+			c.Docs = append(c.Docs, "objectPath: "+objectPath)
+			c.ObjectPath = ""
+		}
+
+		constructors2 = append(constructors2, c)
 	}
 
-	// freely definable
-	if strings.Contains(objectPath, "freely") {
-		c.ObjectPath = ""
-		c.Docs = append(c.Docs, "objectPath: "+objectPath)
-		return
-	}
-
-	// <application dependent>
-	if strings.HasPrefix(objectPath, "<application") {
-		c.Docs = append(c.Docs, "objectPath: "+objectPath)
-		c.ObjectPath = ""
-		return
-	}
-
+	return constructors2
 }
