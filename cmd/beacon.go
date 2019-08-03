68ae0036d001b4233d0e8fd40542779bb003cdf2
Luca Capra <luca.capra@gmail.com>
2019-08-03 08:20:10 +0200

    rename to beacon example

---
+++ cmd/beacon.go
@@ -0,0 +1,53 @@
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	beacon_example "github.com/muka/go-bluetooth/examples/beacon"
+	"github.com/spf13/cobra"
+)
+
+// beaconCmd represents the beacon command
+var beaconCmd = &cobra.Command{
+	Use:   "beacon",
+	Short: "Advertising example",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		adapterID, err := cmd.Flags().GetString("adapterID")
+		if err != nil {
+			fail(err)
+		}
+
+		if len(args) == 0 {
+			failArg("type: ibeacon or eddystone")
+		}
+
+		fail(beacon_example.Run(args[0], adapterID))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(beaconCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// beaconCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// beaconCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ devices/sensortag/st.go
@@ -0,0 +1,6 @@
+package sensortag
+
+func getOptions() map[string]interface{} {
+	options := make(map[string]interface{})
+	return options
+}
