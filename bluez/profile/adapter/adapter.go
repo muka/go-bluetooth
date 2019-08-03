d2e2c8fbef20758ebe2c043360baa71391620090
Luca Capra <luca.capra@gmail.com>
2019-08-03 07:51:42 +0200

    added adapter discovery filter

---
+++ bluez/profile/adapter/adapter.go
@@ -0,0 +1,81 @@
+package adapter
+
+const (
+	DiscoveryFilterTransportAuto  = "auto"
+	DiscoveryFilterTransportBrEdr = "bredr"
+	DiscoveryFilterTransportLE    = "le"
+)
+
+type DiscoveryFilter struct {
+
+	// Filter by service UUIDs, empty means match
+	// _any_ UUID.
+	// When a remote device is found that advertises
+	// any UUID from UUIDs, it will be reported if:
+	// - Pathloss and RSSI are both empty.
+	// - only Pathloss param is set, device advertise
+	// TX pwer, and computed pathloss is less than
+	// Pathloss param.
+	// - only RSSI param is set, and received RSSI is
+	// higher than RSSI param.
+	UUIDs []string
+
+	// RSSI threshold value.
+	// PropertiesChanged signals will be emitted
+	// for already existing Device objects, with
+	// updated RSSI value. If one or more discovery
+	// filters have been set, the RSSI delta-threshold,
+	// that is imposed by StartDiscovery by default,
+	// will not be applied.
+	RSSI int16
+
+	// Pathloss threshold value.
+	// PropertiesChanged signals will be emitted
+	// for already existing Device objects, with
+	// updated Pathloss value.
+	Pathloss uint16
+
+	// string Transport (Default "auto")
+	// Transport parameter determines the type of
+	// scan.
+	// Possible values:
+	// "auto"	- interleaved scan
+	// "bredr"	- BR/EDR inquiry
+	// "le"	- LE scan only
+	// If "le" or "bredr" Transport is requested,
+	// and the controller doesn't support it,
+	// org.bluez.Error.Failed error will be returned.
+	// If "auto" transport is requested, scan will use
+	// LE, BREDR, or both, depending on what's
+	// currently enabled on the controller.
+	Transport string
+
+	// bool DuplicateData (Default: true)
+	// Disables duplicate detection of advertisement
+	// data.
+	DuplicateData bool
+}
+
+func (a *DiscoveryFilter) uuidExists(uuid string) bool {
+	for _, uiid1 := range a.UUIDs {
+		if uiid1 == uuid {
+			return true
+		}
+	}
+	return false
+}
+
+func (a *DiscoveryFilter) AddUUIDs(uuids ...string) {
+	for _, uuid := range uuids {
+		if !a.uuidExists(uuid) {
+			a.UUIDs = append(a.UUIDs, uuid)
+		}
+	}
+}
+
+func NewDiscoveryFilter() DiscoveryFilter {
+	return DiscoveryFilter{
+		// default true
+		DuplicateData: true,
+	}
+}
